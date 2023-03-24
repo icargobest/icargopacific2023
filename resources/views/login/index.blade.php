5a269d9d0313b80074dfb0ecebfb0bb3a4ec8f1d
micodecena <micodecena2901@gmail.com>
2023-03-24 09:27:48 +0800

    added created pages

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <!-- MDB -->
+    <link rel="stylesheet" href="/css/mdb.min.css" />
+</head>
+<body>
+    <section class="vh-100" style="background-color: #eee;">
+        <div class="container py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col col-xl-10">
+              <div class="card" style="border-radius: 1rem;">
+                <div class="row g-0">
+                  <div class="col-md-6 col-lg-5 d-none d-md-block" >
+                    <!--Image from web
+                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                      alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;">
+                    -->
+                    <img src="/img/be-our-partner-1.jpeg"
+                      alt="login form" class="img-fluid w-100" style="border-radius: 1rem 0 0 1rem;" />
+                  </div>
+                  <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                    <div class="card-body p-4 p-lg-5 text-black">
+      
+                      <form>
+      
+                        <div class="d-flex align-items-center mb-3 pb-1">
+                          <!--
+                          <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                          -->
+                          <div class="me-3"><img src="/img/icargo-logo-1.jpg" style="max-height:70px;""></div>
+                          <div class="col-lg-6"><span class="h1 fw-bold mb-0">Icargo Pacific</span></div>
+                          
+                        </div>
+      
+                        <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
+      
+                        <div class="form-outline mb-4">
+                          <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                          <label class="form-label" for="form2Example17">Email address</label>
+                        </div>
+      
+                        <div class="form-outline mb-4">
+                          <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                          <label class="form-label" for="form2Example27">Password</label>
+                        </div>
+      
+                        <div class="pt-1 mb-4">
+                          <a href="/dashboard">
+                          <button class="btn btn-primary btn-lg btn-block" type="button">Login</button>
+                          </a>
+                        </div>
+      
+                        <a class="small text-muted" href="/forgot-password">Forgot password?</a>
+                        <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? 
+                          <a href="/register" style="color: #393f81;" >Register here</a></p>
+                        <a href="/terms-of-use.html" class="small text-muted">Terms of use.</a>
+                        <a href="/privacy-policy.html" class="small text-muted">Privacy policy</a>
+                      </form>
+      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    <!--Bootstrap-->
+    <script src="/js/bootstrap.bundle.js"></script>
+    <!-- MDB -->
+    <script type="text/javascript" src="/js/mdb.min.js"></script>
+</body>
+</html>
