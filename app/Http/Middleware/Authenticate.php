768254da627d42ed34e2b71e0e98fe301fcd425d
Bianca Aquino <lyahbianca.aquino04@gmail.com>
2023-03-28 17:59:54 +0800

    Added login first validation to unauthenticated user

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,8 +1,12 @@
 <?php
+
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class Authenticate extends Middleware
 {
@@ -12,10 +16,13 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    protected function redirectTo($request)
+    public function handle($request, Closure $next, ...$guards)
     {
-        if (!$request->expectsJson()) {
-            return route('login');
+        if (!Auth::check()) {
+            session()->flash('error', 'Please login to access this page.');
+            return redirect()->route('login');
         }
+    
+        return $next($request);
     }
 }
