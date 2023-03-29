71fda6732512773c65f80c6f5d0edc7ca2a087ed
micodecena <micodecena2901@gmail.com>
2023-03-29 12:28:34 +0800

    adjustments

--- resources/views/partials/admin-nav.blade.php
+++ resources/views/partials/admin-nav.blade.php
@@ -14,7 +14,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
     <!-- MDB -->
     <link rel="stylesheet" href="/css/mdb.min.css" />
-
+    <!-- tempwaybill -->
+    <link rel="stylesheet" href="/css/waybilltemp.css" />
 
 
 
