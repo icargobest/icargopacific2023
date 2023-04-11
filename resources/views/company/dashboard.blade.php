ea4725323a6cdbc18efeed2ff68f893f7fbc36e5
adriennecarlocortez <cortez.adrienne00@gmail.com>
2023-04-11 15:31:43 +0800

    dashboard

--- resources/views/company/dashboard.blade.php
+++ resources/views/company/dashboard.blade.php
@@ -10,7 +10,7 @@
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                         <a class="nav-link" href="/company/dashboard">Home</a>
-                        </li>git push origin BRANCH MO
+                        </li>
                         <a class="nav-link @if(isset($dashboard)){{$dashboard}}@endif" href="/dashboard">Dashboard</a>
                         </li>
                         <li class="nav-item">
