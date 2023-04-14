30699df2f49c19292c9814348aa89a598c93044e
ABH-Minon <alexandreeminion@gmail.com>
2023-04-14 11:41:47 +0800

    changed dispatcher path

--- app/Http/Controllers/DispatcherQrScannerController.php
+++ app/Http/Controllers/DispatcherQrScannerController.php
@@ -9,7 +9,7 @@ class DispatcherQrScannerController extends Controller
     // Function to show the page we want to log in by scanner of QR code
     public function index(Request $request)
     {
-        return view('dispatcher.dispatcher');
+        return view('company.dispatcher.dispatcher');
     }
 
     // Function to allow the user to log in or not log in that is done by scanner of QR code
