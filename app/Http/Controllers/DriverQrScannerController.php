ac2338ec17e63406191d69021fe6941046a8fb6b
ABH-Minon <alexandreeminion@gmail.com>
2023-04-20 13:19:00 +0800

    DispatcherQrScannerController connected to Shipment

--- app/Http/Controllers/DriverQrScannerController.php
+++ app/Http/Controllers/DriverQrScannerController.php
@@ -14,36 +14,36 @@ class DriverQrScannerController extends Controller
 
     // Function to allow the user to log in or not log in that is done by scanner of QR code
     public function checkUser(Request $request)
-{
-    $result = 0;
-    $status = null;
-    $tracking_number = null;
-    $id = null;
-    $user_id = null;
+    {
+        $result = 0;
+        $status = null;
+        $tracking_number = null;
+        $id = null;
+        $user_id = null;
 
-    if ($request->data) {
-        $data = explode('-', $request->data);
-        if (count($data) == 2) {
-            $user_id = trim($data[0]);
-            $id = trim($data[1]);
-        } else if (count($data) == 3) {
-            $user_id = trim($data[0]);
-            $tracking_number = trim($data[1]);
-            $id = trim($data[2]);
-        }
-        $shipment = Shipment::where('user_id', $user_id)->where('id', $id)->first();
-        if ($shipment) {
-            if ($tracking_number && $shipment->tracking_number != $tracking_number) {
-                $result = 0;
-            } else {
-                $result = 1;
-                $tracking_number = $shipment->tracking_number;
-                $status = $shipment->status;
+        if ($request->data) {
+            $data = explode('-', $request->data);
+            if (count($data) == 2) {
+                $user_id = trim($data[0]);
+                $id = trim($data[1]);
+            } else if (count($data) == 3) {
+                $user_id = trim($data[0]);
+                $tracking_number = trim($data[1]);
+                $id = trim($data[2]);
+            }
+            $shipment = Shipment::where('user_id', $user_id)->where('id', $id)->first();
+            if ($shipment) {
+                if ($tracking_number && $shipment->tracking_number != $tracking_number) {
+                    $result = 0;
+                } else {
+                    $result = 1;
+                    $tracking_number = $shipment->tracking_number;
+                    $status = $shipment->status;
+                }
             }
         }
+        return response()->json(['result' => $result, 'status' => $status, 'tracking_number' => $tracking_number, 'id' => $id]);
     }
-    return response()->json(['result' => $result, 'status' => $status, 'tracking_number' => $tracking_number, 'id' => $id]);
-}
 
 
     public function updatePickup(Request $request)
