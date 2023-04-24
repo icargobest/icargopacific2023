abdf6e4664cb93c57d0042ec11300ed9b953d854
AeronChristTalaro <aeronchrist123@gmail.com>
2023-04-24 11:46:02 +0800

    fix create modal

--- resources/views/company/drivers/create.blade.php
+++ resources/views/company/drivers/create.blade.php
@@ -1,110 +1,112 @@
-<button type="button" class="btn btn-success" data-mdb-toggle="modal" data-mdb-target="#createModal">
-    Add Driver
-</button>
-<div class="modal top fade" id="createModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-mdb-backdrop="static" data-mdb-keyboard="true">
-    <div class="modal-dialog ">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Add Driver</h5>
-          <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
-        </div>
+<!-- Modal -->
+<div class="modal top fade" id="addDriverModal" tabindex="-1" aria-labelledby="addDriverModal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="addDriverModal">Add Driver</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+    
+      <div class="modal-divider"></div>
+
         <div class="modal-body">
           <form action="{{ route('drivers.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
+
+            <!-- 2 column grid layout with text inputs for the first and last names -->
             <!-- Driver Name -->
             <div class="row mb-4">
-              <div class="col">
-                <div class="form-outline">
-                  <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                  <label class="form-label" for="form6Example1">Driver Name</label>
+                <div class="col">
+                  <div class="form-outline">
+                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                  @error('name')
+                                  <span class="invalid-feedback" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                                  @enderror
+                    <label class="form-label" for="form6Example1">FULL NAME</label>
+                  </div>
                 </div>
               </div>
-            </div>
-
-            <!-- Driver Email -->
+  
+              <!-- Driver Email -->
+              <div class="row mb-4">
+                <div class="col">
+                  <div class="form-outline">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" >
+                                  @error('email')
+                                  <span class="invalid-feedback" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                                  @enderror
+                    <label class="form-label" for="form6Example1">EMAIL</label>
+                  </div>
+                </div>
+              </div>
+  
+              <!-- Driver password -->
+              <div class="row mb-4">
+                <div class="col">
+                  <div class="form-outline">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                  @error('password')
+                                      <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                      </span>
+                                  @enderror
+                    <label class="form-label" for="form6Example1">PASSWORD</label>
+                  </div>
+                </div>
+              </div>
+  
+               <!-- Driver confirm password -->
+               <div class="row mb-4">
+                <div class="col">
+                  <div class="form-outline">
+                    <input type="password" id="form6Example1" name="password_confirmation" class="form-control" required/>
+                    <label class="form-label" for="form6Example1">CONFIRM PASSWORD</label>
+                  </div>
+                </div>
+              </div>
+            
             <div class="row mb-4">
-              <div class="col">
-                <div class="form-outline">
-                  <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" >
+              <label class="form-label" for="vehicle_type"></label>
+              <select type="text" id="form6Example5" name="vehicle_type"style="width:95% !important; margin:auto;border:1px solid #ced4da; height:33.26px; border-radius:0.375rem;padding: 5.12px 12px; color:#828282;"required>
+                <option value="" hidden>VEHICLE TYPE</option>
+                <option value="Motorcycle">Motorcycle</option>
+                <option value="Van">Van</option>
+                <option value="Truck">Truck</option>
+              </select>
+            </div>
 
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                  <label class="form-label" for="form6Example1">Email</label>
+            <div class="form-outline mb-4">
+                <input type="text" id="form6Example4" name="plate_no" class="form-control"required />
+                <label class="form-label" for="form6Example4">PLATE NO.</label>
+            </div>
+
+                  
+            <div class="button-modal-container">
+
+                <div class="leftmodal-button-container">
+                    <button type="reset" class="btn btn-outline-primary" data-mdb-dismiss="modal">
+                        Reset
+                    </button>
                 </div>
-              </div>
-            </div>
+    
+                <div class="rightmodal-button-container">
+    
+                    <button type="submit" class="btn btn-primary" id="addModal2"data-mdb-dismiss="modal">
+                        Save
+                    </button>
 
-            <!-- Driver password -->
-            <div class="row mb-4">
-              <div class="col">
-                <div class="form-outline">
-                  <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Password">
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                  <label class="form-label" for="form6Example1">Password</label>
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">
+                        Back
+                    </button>
                 </div>
-              </div>
-            </div>
 
-             <!-- Driver confirm password -->
-             <div class="row mb-4">
-              <div class="col">
-                <div class="form-outline">
-                  <input type="password" id="form6Example1" name="password_confirmation" class="form-control" required/>
-                  <label class="form-label" for="form6Example1">Confirm Password</label>
-                </div>
-              </div>
-            </div>
-
-            <h4>Driver Details</h4>
-
-            <!-- Vehicle Type -->
-            <div class="row mb-4">
-              <div class="col">
-              <label for="vehicle_type">Vehicle Type</label>
-                <select type="text" id="form6Example5" name="vehicle_type" required>
-                  <option value="" hidden>Select Vehicle Type</option>
-                  <option value="Motorcycle">Motorcycle</option>
-                  <option value="Van">Van</option>
-                  <option value="Truck">Truck</option>
-                </select>
-              </div>
-            </div>
-
-            <!-- Plate No. -->
-            <div class="row mb-4">
-              <div class="col">
-                <div class="form-outline">
-                  <input type="text" id="form6Example1" name="plate_no" class="form-control" required />
-                  <label class="form-label" for="form6Example1">Plate No.</label>
-                </div>
-              </div>
-            </div>
-
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-primary btn-block">
-                  Save
-                </button>
-                <button type="reset" class="btn btn-danger btn-block mb-4">
-                  Clear
-                </button>
-              </div>
           </form>
         </div>
-      </div>
+
     </div>
   </div>
-</div>
 </div>
