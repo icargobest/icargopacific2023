d7dc00360fe5b2d30d151eb6fffc341142fbe0b1
Bianca Aquino <lyahbianca.aquino04@gmail.com>
2023-04-25 00:29:52 +0800

    deleted unecessary validations

--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -14,12 +14,8 @@ class CreateUserRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'updateFullName' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'updateEmail' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'updatePassword' => ['required', 'string', 'min:8', 'confirmed'],
-            // 'contact_no' => ['required', 'int', 'min:11', 'max:11'],
         ];
     }
 
@@ -33,15 +29,6 @@ class CreateUserRequest extends FormRequest
             'email.required' => 'Email field is required.',
             'email.unique' => 'Email address must be unique within the organization',
             'email.email' => 'Email field must be email address.',
-            'updateFullName.required' => 'Name field is required.',
-            'updatePassword.required' => 'Password field is required.',
-            'updatePassword.confirmed' => 'Password does not match.',
-            'updatePassword.min' => 'Password must be a minimum of 8 characters',
-            'updateEmail.required' => 'Email field is required.',
-            'updateEmail.unique' => 'Email address must be unique within the organization',
-            'updateEmail.email' => 'Email field must be email address.',
-            // 'contact_no.required' => 'Contact field is required.',
-            // 'contact_no.max' => 'Contact nuber must be a min and max of 11 characters'
         ];
     } 
 }
