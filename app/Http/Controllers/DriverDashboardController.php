27cdcb4c4cdde22b25beccfb55c782c1275e48c3
Arth <johncarandang571@gmail.com>
2023-04-26 12:00:21 +0800

    Commit

---
+++ app/Http/Controllers/DriverDashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class DriverDashboardController extends Controller
+{
+    public function index()
+    {
+        $dashboard = DB::table('dashboard')->first();
+        return view('driver\dashboard', compact('dashboard'));
+    }
+}
