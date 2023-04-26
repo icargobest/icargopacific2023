0ff535d6891a9303c7d6a92b1905ed11d8e601be
Arth <johncarandang571@gmail.com>
2023-04-26 12:01:42 +0800

    Commit

--- app/Http/Controllers/DispatcherDashboardController.php
+++ app/Http/Controllers/DispatcherDashboardController.php
@@ -11,6 +11,6 @@ class DispatcherDashboardController extends Controller
     public function index()
     {
         $dashboard = DB::table('dashboard')->first();
-        return view('dispatchdashboard', compact('dashboard'));
+        return view('comapny\dispatcher\dashboard', compact('dashboard'));
     }
 }
