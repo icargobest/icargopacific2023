50d52a385c05689d4b13ed38f2d69691f28b8943
JohnMichaelPrila <prila.johnmichael@gmail.com>
2023-05-11 15:13:28 +0800

    LandingPage&UserCard

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -6,7 +6,7 @@
     
             <span class="textPricing-wrapper d-flex flex-column justify-content-center align-items-center">
                 <h4 class="fw-bolder">We've got a <span>perfect</span> plan for you</h4>
-                <h6>Get access to all features of iCARGO PACIFIC</h6>
+                <h6>Digitalized your Logistic Company</h6>
             </span>
         </div>
 
@@ -14,68 +14,78 @@
             
             {{-- FREE TRIAL --}}
 
-            <div class="cardPlan d-flex flex-column align-items-center shadow-sm">
-                <div class="duration w-100 h-50 p-3 d-flex flex-column align-items-center gap-4 ">
+            <div class="cardPlan p-4 d-flex flex-column align-items-center shadow-sm">
+                <div class="duration w-100 p-3 d-flex flex-column align-items-center gap-4 ">
                     <h5>FREE TRIAL!</h5>
-                    <span class="timeDue d-flex align-items-center gap-2">
+                    <span class="timeDue mb-3 d-flex align-items-center gap-2">
                         <div class="d-flex justify-content-center align-items-center"><span>30</span></div>DAYS
                     </span>
                 </div>
-                <div class="price w-100 h-50 p-4 d-flex flex-column align-items-center gap-5">
-                    <h3 class="pt-2 fw-bold"><span>₱ </span>FREE</h3>
-                    <div class="buttonHolder w-100">
-                        <button>START</button>
+                <div class="price w-100 h-75 pt-4 d-flex flex-column align-items-center gap-4">
+                    <ul>
+                        <li>Free 1 month trial</li>
+                        <li>Free waybill printer</li>
+                        <li>Free training and demo</li>
+                        <li>Free payment and integration platform</li>
+                    </ul>
+                    <div class="d-flex flex-column w-100 align-items-center gap-3">
+                        <h3 class="pt-2 fw-bold"><span>₱ </span>FREE</h3>
+                        <div class="buttonHolder w-100">
+                            <a href="/company_registration"><button>START</button></a>
+                        </div>
                     </div>
                 </div>
             </div>
             {{-- PLAN 1 --}}
 
-            <div class="cardPlan d-flex flex-column align-items-center shadow-sm">
-                <div class="duration w-100 h-50 p-3 d-flex flex-column align-items-center gap-4 ">
-                    <h5>BASIC</h5>
-                    <span class="timeDue d-flex align-items-center gap-2">
+            <div class="cardPlan cardPlanBest p-4 d-flex flex-column align-items-center shadow-sm">
+                <div class="duration w-100 p-3 d-flex flex-column align-items-center gap-4 ">
+                    <h5>STANDARD</h5>
+                    <span class="timeDue mb-3 d-flex align-items-center gap-2">
                         <div class="d-flex justify-content-center align-items-center"><span>1</span></div>MONTH
                     </span>
                 </div>
-                <div class="price w-100 h-50 p-4 d-flex flex-column align-items-center gap-5">
-                    <h3 class="pt-2 fw-bold"><span>₱ </span>----</h3>
-                    <div class="buttonHolder w-100">
-                        <button>SUBSCRIBE</button>
+                <div class="price w-100 h-75 pt-4 d-flex flex-column align-items-center gap-4">
+                    <ul>
+                        <li>Cloud Tracking and Monitoring</li>
+                        <li>5,000pcs Printing of waybill a month</li>
+                        <li>Advance Freight Forwarding</li>
+                        <li>Logistic software management</li>
+                    </ul>
+                    <div class="d-flex flex-column w-100 align-items-center gap-3">
+                        <h3 class="pt-2 fw-bold"><span>₱ </span>6,999</h3>
+                        <div class="buttonHolder w-100">
+                            <a href="/company_registration"><button>START</button></a>
+                        </div>
                     </div>
                 </div>
             </div>
             {{-- PLAN 2 --}}
 
-            <div class="cardPlan d-flex flex-column align-items-center shadow-sm">
-                <div class="duration w-100 h-50 p-3 d-flex flex-column align-items-center gap-4 ">
-                    <h5>STANDARD</h5>
-                    <span class="timeDue d-flex align-items-center gap-2">
-                        <div class="d-flex justify-content-center align-items-center"><span>3</span></div>MONTHS
+            <div class="cardPlan p-4 d-flex flex-column align-items-center shadow-sm">
+                <div class="duration w-100 p-3 d-flex flex-column align-items-center gap-4 ">
+                    <h5>PREMIUM</h5>
+                    <span class="timeDue mb-3 d-flex align-items-center gap-2">
+                        <div class="d-flex justify-content-center align-items-center"><span>1</span></div>MONTH
                     </span>
                 </div>
-                <div class="price w-100 h-50 p-4 d-flex flex-column align-items-center gap-5">
-                    <h3 class="pt-2 fw-bold"><span>₱ </span>----</h3>
-                    <div class="buttonHolder w-100">
-                        <button>SUBSCRIBE</button>
+                <div class="price w-100 h-75 pt-4 d-flex flex-column align-items-center gap-4">
+                    <ul>
+                        <li>Cloud tracking and monitoring</li>
+                        <li>Unlimited printing of waybill</li>
+                        <li>Advance freight forwarding</li>
+                        <li>Logistic software management</li>
+                        <li>ICargo Merchants and Customer</li>
+                    </ul>
+                    <div class="d-flex flex-column w-100 align-items-center gap-3">
+                        <h3 class="pt-2 fw-bold"><span>₱ </span>9,999</h3>
+                        <div class="buttonHolder w-100">
+                            <a href="/company_registration"><button>START</button></a>
+                        </div>
                     </div>
                 </div>
             </div>
-            {{-- PLAN 2 --}}
-
-            <div class="cardPlan d-flex flex-column align-items-center shadow-sm">
-                <div class="duration w-100 h-50 p-3 d-flex flex-column align-items-center gap-4 ">
-                    <h5>ULTIMATE</h5>
-                    <span class="timeDue d-flex align-items-center gap-2">
-                        <div class="d-flex justify-content-center align-items-center"><span>1</span></div>PAYMENT
-                    </span>
-                </div>
-                <div class="price w-100 h-50 p-4 d-flex flex-column align-items-center gap-5">
-                    <h3 class="pt-2 fw-bold"><span>₱ </span>----</h3>
-                    <div class="buttonHolder w-100">
-                        <button>SUBSCRIBE</button>
-                    </div>
-                </div>
-            </div>
+            
 
         </div>
     
