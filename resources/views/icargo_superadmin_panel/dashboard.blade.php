b4e78fb500e2316fdb2568556598a52a7e95b2ea
Arth <johncarandang571@gmail.com>
2023-05-15 16:33:59 +0800

    Dashboard Backend Multi Auth FIX

--- resources/views/icargo_superadmin_panel/dashboard.blade.php
+++ resources/views/icargo_superadmin_panel/dashboard.blade.php
@@ -1,8 +1,14 @@
-<title>Company | Dashboard</title>
+<title>SuperAdmin | Dashboard</title>
 
 
 @extends('layouts.app')
-@include('partials.navigationSuperAdmin')
+@extends('layouts.chart')
+@include('partials.navigationCompany',['dashboard' => "nav-selected"])
+
+{{-- @extends('layouts.chart') --}}
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+@section('title', 'Monthly Income')
 
 @section('content')
 <nav class="navbar navbar-light bg-light">
@@ -18,4 +24,33 @@
         </div>
     </div>
   
-@endsection
+    <body>
+        <h1>Registered Companies: {{ $companycount }}</h1>
+        <h1>Registered Users: {{ $usercount }}</h1>
+    </body>
+<script type="text/javascript">
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawMonthlyChart);
+
+    function drawMonthlyChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Month', 'Income'],
+            @foreach ($incomes as $income)
+            ['{{ date('M', strtotime($income->date)) }}', {{ $income->amount }}],
+            @endforeach
+        ]);
+
+        var options = {
+            title: 'Monthly Income',
+            curveType: 'function',
+            legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('monthly_chart_div'));
+
+        chart.draw(data, options);
+    }
+</script>
+
+
+
